41cea7666bf86f3f71a9e85609b8dcb423189171
GoshaNice <34185262+GoshaNice@users.noreply.github.com>
2022-05-17 23:51:31 +0300

    Create exp3.py

---
+++ src/exp3.py
@@ -0,0 +1,34 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+
+dataset_gosha = pd.read_csv('data/result_Gosha.csv')
+dataset_david = pd.read_csv('data/result_David.csv')
+dataset_ksenia = pd.read_csv('data/result_Ksenia.csv')
+dataset_danila = pd.read_csv('data/result_Danila.csv')
+dataset_225 = pd.read_csv('data/result_225.csv')
+
+dataset = dataset_gosha.append(dataset_david)
+dataset = dataset.append(dataset_ksenia)
+dataset = dataset.append(dataset_danila)
+dataset = dataset.append(dataset_225)
+
+
+X = dataset[['in_sigma_m', 'in_sigma_w']]
+y = dataset['alpha'] # - для alpha
+#y = dataset['beta'] - для beta
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+from sklearn import metrics
+
+print(f'coefficients : {regressor.coef_}')
+print('Mean Absolute Error: ', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error: ', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Absolute Error: ', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
